92b80b35d5667a17c4662626248cbd302b449d4a
SerhatErdogan-r0622581 <serhaterdogan2011@hotmail.com>
2018-08-10 10:45:45 +0200

    evaluation

--- herexamenOOO/src/controller/Evaluation.java
+++ herexamenOOO/src/controller/Evaluation.java
@@ -1,5 +1,36 @@
 package controller;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Evaluation {
+
+	String result = "";
+	InputStream inputStream;
+	
+	public String getPropValue() throws IOException {
+		try {
+			Properties prop = new Properties();
+			String propFileName = "evaluation.properties";
+
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file " + propFileName + " not found in classpath");
+			}
+			String feedback = prop.getProperty("feedback");
+			
+			result = feedback;
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+		return result;
+	}
 
 }
